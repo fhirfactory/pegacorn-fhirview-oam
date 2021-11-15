7da5e9e837021249df1f37986e9ec719aaa27cc8
kelskye <kelskye@gmail.com>
2021-11-15 19:50:19 +1100

    Preliminary display of data from a record

--- pm/src/Task.js
+++ pm/src/Task.js
@@ -2,10 +2,33 @@ import * as React from "react";
 import { Show } from 'react-admin';
 
 export const TaskShow = (props) => { 
-
-
     return (
         <Show title="Show Task" mutationMode="pessimistic" {...props}>
+            <SimpleShowLayout>
+                <TextField source="taskId" />
+                <TextField source="taskFocus" />
+                <ArrayField source="taskBeneficiary">
+                    <Datagrid>
+                        <TextField source="given" />
+                        <TextField source="surname" />
+                        <TextField source="otherGiven" />
+                        <DateField source="dateOfBirth" />
+                        <TextField source="medicalRecordNumber" />
+                    </Datagrid>
+                </ArrayField>
+                <TextField source="taskEncounter" />
+                <TextField source="taskJourney" />
+                <TextField source="taskInput" />
+                <TextField source="taskOutput" />
+                <DateField source="taskPeriod.startInstant" />
+                <DateField source="taskPeriod.finishInstant" />
+                <TextField source="taskStatus" />
+                <TextField source="taskStatusReason" />
+                <TextField source="taskTriggerType" />
+                <TextField source="taskTriggerId" />
+                <TextField source="taskFulfillmentLocation" />
+                {/* TODO Subtasks */}
+            </SimpleShowLayout>
         </Show>
     )
 };
