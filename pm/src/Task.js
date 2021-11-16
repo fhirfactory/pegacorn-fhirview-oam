3dedb01d422b7fb5c40741f11001177f89c4ff83
kelskye <kelskye@gmail.com>
2021-11-16 17:23:57 +1100

    A more complete version of the Task.js

--- pm/src/Task.js
+++ pm/src/Task.js
@@ -1,34 +1,71 @@
 import * as React from "react";
-import { Show } from 'react-admin';
+import { 
+    Show, 
+    TabbedShowLayout, 
+    Tab, 
+    TextField, 
+    ArrayField, 
+    SingleFieldList, 
+    DateField } from 'react-admin';
 
 export const TaskShow = (props) => { 
     return (
         <Show title="Show Task" mutationMode="pessimistic" {...props}>
-            <SimpleShowLayout>
-                <TextField source="taskId" />
-                <TextField source="taskFocus" />
-                <ArrayField source="taskBeneficiary">
-                    <Datagrid>
-                        <TextField source="given" />
-                        <TextField source="surname" />
-                        <TextField source="otherGiven" />
-                        <DateField source="dateOfBirth" />
-                        <TextField source="medicalRecordNumber" />
-                    </Datagrid>
-                </ArrayField>
-                <TextField source="taskEncounter" />
-                <TextField source="taskJourney" />
-                <TextField source="taskInput" />
-                <TextField source="taskOutput" />
-                <DateField source="taskPeriod.startInstant" />
-                <DateField source="taskPeriod.finishInstant" />
-                <TextField source="taskStatus" />
-                <TextField source="taskStatusReason" />
-                <TextField source="taskTriggerType" />
-                <TextField source="taskTriggerId" />
-                <TextField source="taskFulfillmentLocation" />
-                {/* TODO Subtasks */}
-            </SimpleShowLayout>
+            <TabbedShowLayout>
+                <Tab label="details">
+                    <TextField source="taskId" />
+                    <TextField source="taskFocus" />
+                    <ArrayField source="taskBeneficiary">
+                        <SingleFieldList>
+                            <TextField source="given" />
+                            <TextField source="surname" />
+                            <TextField source="otherGiven" />
+                            <DateField source="dateOfBirth" />
+                            <TextField source="medicalRecordNumber" />
+                        </SingleFieldList>
+                    </ArrayField>
+                    <TextField source="taskEncounter" />
+                    {/* Needs to change to reflect array */}
+                    <TextField source="taskJourney" /> 
+                    <TextField source="taskInput" />
+                    <TextField source="taskOutput" />
+                    <DateField source="taskPeriod.startInstant" />
+                    <DateField source="taskPeriod.finishInstant" />
+                    <TextField source="taskStatus" />
+                    <TextField source="taskStatusReason" />
+                    <TextField source="taskTriggerType" />
+                    <TextField source="taskTriggerId" />
+                    <TextField source="taskFulfillmentLocation" />
+                </Tab>
+                <Tab label="Subtasks">
+                    <ArrayField source="subTasks">
+                        <SingleFieldList>
+                            <TextField source="taskId" />
+                            <TextField source="taskFocus" />
+                            <ArrayField source="taskBeneficiary">
+                                <SingleFieldList>
+                                    <TextField source="given" />
+                                    <TextField source="surname" />
+                                    <TextField source="otherGiven" />
+                                    <DateField source="dateOfBirth" />
+                                    <TextField source="medicalRecordNumber" />
+                                </SingleFieldList>
+                            </ArrayField>
+                            <TextField source="taskEncounter" />
+                            <TextField source="taskJourney" /> 
+                            <TextField source="taskInput" />
+                            <TextField source="taskOutput" />
+                            <DateField source="taskPeriod.startInstant" />
+                            <DateField source="taskPeriod.finishInstant" />
+                            <TextField source="taskStatus" />
+                            <TextField source="taskStatusReason" />
+                            <TextField source="taskTriggerType" />
+                            <TextField source="taskTriggerId" />
+                            <TextField source="taskFulfillmentLocation" />
+                        </SingleFieldList>
+                    </ArrayField>
+                </Tab>
+            </TabbedShowLayout>
         </Show>
     )
 };
